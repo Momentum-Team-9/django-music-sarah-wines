6301beadd7e8098039ae5a8fe8138ea2ed576abe
sarah-wines <sarah.lodico.wines@gmail.com>
2021-08-14 23:31:23 -0400

    connects to album details

--- Album/views.py
+++ Album/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album, Track
 # Create your views here.
 def list_albums(request):
     albums = Album.objects.all()
     return render(request, 'albums/list_albums.html', {'albums': albums})
+
+def show_album(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    return render(
+        request,
+        "albums/show_album.html",
+        {"album": album, "pk": pk},
+    )
